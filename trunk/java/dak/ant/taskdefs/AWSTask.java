0b45bf87482c79984a4fb9144395160fbd4dcdf0
dkavanagh <dkavanagh@ab08a224-064b-0410-bd1f-055cc1ae1b14>
2008-07-23 13:27:11 +0000

    can now set ACL on objects to be public

--- trunk/java/dak/ant/taskdefs/AWSTask.java
+++ trunk/java/dak/ant/taskdefs/AWSTask.java
@@ -1,11 +1,9 @@
 package dak.ant.taskdefs;
 
 import java.util.Iterator;
-import java.util.Vector;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.taskdefs.MatchingTask;
-import org.apache.tools.ant.types.FileSet;
 
 /**
  * <p>This class provides basic S3 actions as an Ant task</p>
@@ -18,7 +16,6 @@ public abstract class AWSTask extends MatchingTask {
 	protected boolean failOnError = false;
 	protected String accessId;
 	protected String secretKey;
-	protected Vector filesets = new Vector();
 	
 	public void setAccessId(String accessId) {
 		this.accessId = accessId;
@@ -36,10 +33,6 @@ public abstract class AWSTask extends MatchingTask {
 		this.failOnError = failOnError;
 	}
 
-	public void addFileset(FileSet set) {
-		filesets.addElement(set);
-	}
-
 	/**
      * Check that all required attributes have been set and nothing
      * silly has been entered.
@@ -52,9 +45,5 @@ public abstract class AWSTask extends MatchingTask {
 			throw new BuildException("accessId must be set");
 		if (secretKey == null)
 			throw new BuildException("secretKey must be set");
-		if (filesets == null) {
-			System.err.println("No fileset was provided, doing nothing");
-			return;
-		}
     }
 }
