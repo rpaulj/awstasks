39bb2cd935781cbf8e068cf2357b747cf9abfa51
tony.seebregts <tony.seebregts@gmail.com>
2011-03-07 18:36:27 +0000

    1. Imported S3Copy, S3FileSet and S3File.

---
+++ java/dak/ant/taskdefs/S3Copy.java
@@ -0,0 +1,143 @@
+package dak.ant.taskdefs;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+import org.apache.tools.ant.BuildException;
+import org.jets3t.service.S3Service;
+import org.jets3t.service.S3ServiceException;
+import org.jets3t.service.impl.rest.httpclient.RestS3Service;
+import org.jets3t.service.model.S3Bucket;
+import org.jets3t.service.model.S3Object;
+import org.jets3t.service.security.AWSCredentials;
+
+import dak.ant.types.S3FileSet;
+
+/**
+ * N.B. Only bucket to bucket copy as this is all i need, but 
+ * It should be extendible if/when required. 
+ * @author chris
+ *
+ */
+public class S3Copy extends S3FileSetTask {
+    private String toBucket;
+    private String fromBucket;
+    private String prefix;
+    private Pattern filePattern;
+    
+    public void setToBucket(String bucket) {
+        this.toBucket = bucket;
+    }
+    
+    public void setFromBucket(String bucket) {
+        this.fromBucket = bucket;
+    }
+    
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+    
+    public void setFileRegex(String regex) {
+       this.filePattern = Pattern.compile(regex);
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        checkParameters();
+        try {
+           if( isFileSetMode( ) )
+              for ( S3FileSet fileSet : fileSets ) {
+                 doFileSetCopy( fileSet );               
+              }
+           else   
+             doNonFileSetCopy( );
+        }
+        catch (Exception e) {
+            throw new BuildException(e);
+        }
+    }
+
+   @SuppressWarnings("unchecked")
+   private void doFileSetCopy( S3FileSet fileSet ) throws S3ServiceException {
+      
+      AWSCredentials credentials = fileSet.getCredentials( );
+      S3Service s3 = new RestS3Service(credentials);
+       
+      for ( Iterator<File> i = fileSet.iterator( ); i.hasNext( ); ) {
+         File file = i.next( );
+         
+         String key = file.getName( );
+         
+         S3Object object = new S3Object( key );
+         
+         s3.copyObject( fileSet.getBucket( ), key, toBucket, object, true );
+         
+         if (verbose)
+            log("copied " + object.getKey());
+      }  
+   }
+
+   private void doNonFileSetCopy( ) throws S3ServiceException {
+      
+      AWSCredentials credentials = new AWSCredentials(accessId, secretKey);
+      S3Service s3 = new RestS3Service(credentials);
+      S3Bucket fromBucket = new S3Bucket(this.fromBucket);
+      
+      S3Object[] objectListing;
+      if (prefix != null)
+          objectListing = s3.listObjects(fromBucket, prefix, null);
+      else
+          objectListing = s3.listObjects(fromBucket);
+      for (S3Object object : objectListing) {
+          if (objectMatches(object)) {
+             s3.copyObject( this.fromBucket, object.getKey( ), toBucket, object, true );
+              if (verbose)
+                  log("copied " + object.getKey());
+          }
+      }
+   }
+    
+    protected void checkParameters() throws BuildException {
+           
+       if( toBucket == null ) {
+          throw new BuildException("toBucket must be set");
+       }
+      
+       if( !isFileSetMode() ) {
+          super.checkParameters( );
+          
+          if( fromBucket == null ) {
+             throw new BuildException("fromBucket must be set");
+          }
+          
+          return;
+       }
+       // S3FileSet(s) given
+       // S3FileSet contains all of these
+       if( accessId != null ) {
+          throw new BuildException("cannot set s3FileSet and accessId");
+       }
+       if( secretKey != null ) {
+          throw new BuildException("cannot set s3FileSet and secretKey");
+       }
+       if( fromBucket != null ) {
+          throw new BuildException("cannot set s3FileSet and bucket");
+       }
+       if( prefix != null ) {
+          throw new BuildException("cannot set s3FileSet and prefix");
+       }
+       if( filePattern != null ) {
+          throw new BuildException("cannot set s3FileSet and filePattern");
+       }
+   }
+    
+    private boolean objectMatches(S3Object object) {
+        String toCompare = prefix == null ? 
+                object.getKey() : object.getKey().substring(prefix.length());
+        if (filePattern == null)
+            return true;
+        else
+            return filePattern.matcher(toCompare).matches();
+    }
+}
---
+++ java/dak/ant/types/S3File.java
@@ -0,0 +1,51 @@
+package dak.ant.typedefs;
+
+import java.io.File;
+
+/**
+ * Attempt to make S3 objects compatible with ant file selectors.
+ * This could probably be done a lot better. One thing to explore would
+ * be the URI constructor - this might allow bucket to be used as part
+ * of the file path which might make some of the S3 operations easier
+ * to handle? Or maybe harder!
+ * 
+ * It might be here that you could plug in a File System implementation that
+ * uses any of a number of naming schemes to simulate directories within S3 -
+ * a number of tools do this and it'd be nice to be able to read their
+ * output. 
+ * 
+ * I should probably handle isAbsolute some how.
+ * @author chris
+ *
+ */
+public class S3File extends File
+{
+   private static final long serialVersionUID = 1825082919531083517L;
+
+   private long lastModified;
+   
+   public S3File( String pathname )
+   {
+      super( pathname );
+   }
+
+   @Override
+   public boolean isDirectory( )
+   {
+      return false; // currently no such thing as a directory.
+   }
+   
+   @Override
+   public boolean setLastModified( long time ) {
+      lastModified = time;
+      return true;
+   }
+   
+   @Override
+   public long lastModified( )
+   {
+      return lastModified;
+   }
+   
+   
+}
